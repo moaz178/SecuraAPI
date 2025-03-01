76032d9952adfc9e4a0d2b6ad3a29a271761e67f
moazahmad <moaz.ahmad@royalcyber.com>
2025-03-02 01:48:32 +0500

    with outlet

--- src/features/Layout/Layout.jsx
+++ src/features/Layout/Layout.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import "./Layout.css";
+
 const Layout = () => {
   return (
     <div>
       <Navbar />
-      <div className="side-menu">
-        <Sidebar />
+      <div className="layout-container">
+        <div className="side-menu">
+          <Sidebar />
+        </div>
+        <div className="content">
+          {/* This is where nested routes will render */}
+          <Outlet />
+        </div>
       </div>
     </div>
   );
